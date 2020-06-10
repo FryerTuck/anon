1c7307dc2f8d46261d4734fa281c47f2b1852260
argon <kindhacker@protonmail.com>
2020-06-10 21:03:50 +0200

    AnonDeploy upgrade

--- .anon.dir/Anon/aard.php
+++ .anon.dir/Anon/aard.php
@@ -15,6 +15,7 @@ namespace Anon;
           permit::fubu('clan:sudo,lead,gang,geek'); $post=knob($_POST);
           if(!$purl){$purl=$post->purl; $vars=$post->vars;}; if(!isKnob($vars)){$vars=knob();};
           expect::purl($purl); $info=path::info($purl);
+          signal::busy(['with'=>'remoteDeploy','done'=>10]);
 
           $code = pget('$/Anon/base/deploy.php');
           $hash = sha1(encode::b64($code.PROCHASH)); $vars->ck=$hash; // crack this b!tch .. i can do better .. time is short
@@ -25,15 +26,17 @@ namespace Anon;
           $plug->vivify();
 
           if(!$plug->link){$f=$plug->fail; if(!$f){$f="connection failure";}; fail::remoteDeploy($f); exit;};
-          signal::busy(['with'=>'remoteDeploy','done'=>10]);
+          signal::busy(['with'=>'remoteDeploy','done'=>20]);
 
           $done = $plug->delete(['.htaccess','.anon.php','index.php','index.html']);
           if(!$done){fail::remoteDeploy('unable to delete remote auto-handler'); exit;};
-          signal::busy(['with'=>'remoteDeploy','done'=>20]);
+          signal::busy(['with'=>'remoteDeploy','done'=>40]);
 
           $done = $plug->insert(['index.php'=>$code]);
           if(!$done){fail::remoteDeploy('unable to insert remote auto-handler'); exit;};
-          signal::busy(['with'=>'remoteDeploy','done'=>30]);
+          signal::busy(['with'=>'remoteDeploy','done'=>60]);
+          $done = $plug->delete(['index.php']);
+          signal::busy(['with'=>'remoteDeploy','done'=>80]);
 
           $plug->pacify();
 
