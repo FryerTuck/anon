08dbf2dfa71ffa28b20aaa20fee76727b07a5db3
argon <kindhacker@protonmail.com>
2020-06-12 08:36:07 +0200

    user void fix

--- .anon.dir/User/tool/user/host.php
+++ .anon.dir/User/tool/user/host.php
@@ -112,7 +112,7 @@ $export=function($a,$u,$d)
    {
       if(!userDoes("sudo lead")){ekko("only leaders and sudoers can do that"); exit;};
       if(!isee($h)){ekko("user `$u` is undefined"); exit;};  $s=find::sesnByUser($u);
-      signal::ClientReboot("user deleted","#$u");
+      signal::ClientReboot("user deleted","#$u"); wait(500);
       if($s){path::void("/Proc/temp/sesn/$s");}; path::void($h);
       ekko(OK);
    };
