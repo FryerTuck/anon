ef3fc1579887052888513d44208813a5b3ba0bbe
argon <kindhacker@protonmail.com>
2020-06-19 14:35:36 +0200

    User initConf fix

--- .anon.dir/User/aard.php
+++ .anon.dir/User/aard.php
@@ -28,7 +28,7 @@ namespace Anon;
       {
           permit::fubu("clan:sudo"); $v=knob($_POST);
           if(!isPass($v->pass)){fail::config("Invalid password. Please try again."); exit;};
-          if(!isPlug($v->mail)){fail::config("Invalid mail-plug. Please try again"); exit;};
+          if(!isPurl($v->mail)){fail::config("Invalid mail-plug. Please try again"); exit;};
           path::make("$/User/data/master/pass",$v->pass);
           path::make("$/Proc/conf/autoMail",$v->mail);
           ekko(OK);
