69e17888a26c5e7dff33a9d0c17740de2b1ea783
argon <kindhacker@protonmail.com>
2020-06-20 18:26:07 +0200

    Temporarily removed ProtectImage conf

--- .anon.dir/Proc/base/ximp.js
+++ .anon.dir/Proc/base/ximp.js
@@ -5,14 +5,14 @@
       {
          let n=create({img:'',src:d}); n.rectInfo((i)=>
          {
-            n.width=i.width; n.height=i.height; let pp,ps,pm; pp=conf.protectImgPath; ps=conf.protectImgSize; pm=conf.protectImgMark; // set vars
-            if(!pp||!ps||!pm){f(n);return}; // no protection .. all 3 these must be defined and valid
-            if(!isList(pp)){pp=[pp];}; let pe=0; let ppm=FALS;  pp.forEach((pi)=>{if(!isText(pi,2)){pe=1}else if(akin(d.path,pi)){ppm=TRUE}});
-            if(pe){fail('invalid protectImgPath value in viewConfig');return}; if(!ppm){f(n);return}; // no protetion
-            if(!isKnob(ps)||!isInum(ps.w)||!isInum(ps.h)){fail('invalid protectImgSize value in viewConfig');return}; // validate
-            if(!isPath(pm)&&!isKnob(pm)){fail('invalid protectImgMark value in viewConfig');return}; // validate
-            if((i.width<=ps.w)&&(i.height<=ps.h)){f(n);return}; // no protection for this image's dimensions
-            n.impose(pm,f);
+            // n.width=i.width; n.height=i.height; let pp,ps,pm; pp=conf.protectImgPath; ps=conf.protectImgSize; pm=conf.protectImgMark; // set vars
+            // if(!pp||!ps||!pm){f(n);return}; // no protection .. all 3 these must be defined and valid
+            // if(!isList(pp)){pp=[pp];}; let pe=0; let ppm=FALS;  pp.forEach((pi)=>{if(!isText(pi,2)){pe=1}else if(akin(d.path,pi)){ppm=TRUE}});
+            // if(pe){fail('invalid protectImgPath value in viewConfig');return}; if(!ppm){f(n);return}; // no protetion
+            // if(!isKnob(ps)||!isInum(ps.w)||!isInum(ps.h)){fail('invalid protectImgSize value in viewConfig');return}; // validate
+            // if(!isPath(pm)&&!isKnob(pm)){fail('invalid protectImgMark value in viewConfig');return}; // validate
+            // if((i.width<=ps.w)&&(i.height<=ps.h)){f(n);return}; // no protection for this image's dimensions
+            // n.impose(pm,f);
          });
       },
 
