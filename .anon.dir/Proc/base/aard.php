d0c382d96692c838ae909a679aed9194022e65c2
argon <kindhacker@protonmail.com>
2020-06-22 12:50:56 +0200

    503 test 6

--- .anon.dir/Proc/base/aard.php
+++ .anon.dir/Proc/base/aard.php
@@ -363,8 +363,6 @@ namespace Anon;
       if($u){$n=mksesn($u); return $n;}; // if all went well, we are still alive .. all is well
       $r=envi('REFERER'); $h=envi('HOST'); $s=(strpos($r,"https://$h")===0);
       if($s){return;}; // key may have expired
-
-if(isset($_GET['test'])){die("test 5");};
       harakiri(wack()); // YOU HAVE DIED
    }
 # ---------------------------------------------------------------------------------------------------------------------------------------------
