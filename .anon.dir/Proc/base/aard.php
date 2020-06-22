5472ef411c69c9782ebbea1fb9475d4d9ded1295
argon <kindhacker@protonmail.com>
2020-06-22 10:32:19 +0200

    boot test 6

--- .anon.dir/Proc/base/aard.php
+++ .anon.dir/Proc/base/aard.php
@@ -354,7 +354,7 @@ namespace Anon;
       $l=array_keys($_COOKIE); if(count($l)<1){return;}; $r=null; $t='/^[a-z0-9]{40}$/'; $c=envi('COREPATH'); $h="$c/Proc/temp/sesn";
       $n=null; do{$n=array_pop($l); if(!test($n,$t)){$n=null; continue;}; if(is_dir("$h/$n")){$r=$n;break;}}while(count($l));
       if($r){return $r;}; // session is cookie-based .. it exists as a live session-dir server-side .. all is well
-if(isset($_GET['test'])){die("test 5");};
+if(isset($_GET['test'])){die("test 6");};
       $s=envi('SCHEME'); $h=envi('HOST'); $p=envi('URI'); $z="Location: $s://{$h}{$p}";
       if($n){kuki($n,null); header($z); exit;}; // bad session key
       $r=kuki('APIKEY'); if(!$r){$r=post('APIKEY');}; if(!$r){$r=envi('APIKEY');}; if(!$r){return;}; // no key
