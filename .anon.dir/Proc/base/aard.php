751bc18601972acaa36a903757ee1dbaf4df27f9
argon <kindhacker@protonmail.com>
2020-06-22 10:29:30 +0200

    boot test 5

--- .anon.dir/Proc/base/aard.php
+++ .anon.dir/Proc/base/aard.php
@@ -351,13 +351,14 @@ namespace Anon;
 # ---------------------------------------------------------------------------------------------------------------------------------------------
    function skey()
    {
+if(isset($_GET['test'])){die("test 5");}; 
       $l=array_keys($_COOKIE); if(count($l)<1){return;}; $r=null; $t='/^[a-z0-9]{40}$/'; $c=envi('COREPATH'); $h="$c/Proc/temp/sesn";
       $n=null; do{$n=array_pop($l); if(!test($n,$t)){$n=null; continue;}; if(is_dir("$h/$n")){$r=$n;break;}}while(count($l));
       if($r){return $r;}; // session is cookie-based .. it exists as a live session-dir server-side .. all is well
       $s=envi('SCHEME'); $h=envi('HOST'); $p=envi('URI'); $z="Location: $s://{$h}{$p}";
       if($n){kuki($n,null); header($z); exit;}; // bad session key
       $r=kuki('APIKEY'); if(!$r){$r=post('APIKEY');}; if(!$r){$r=envi('APIKEY');}; if(!$r){return;}; // no key
-      if(!test($r,$t)){if(isset($_GET['test'])){die("test 5");}; harakiri(wack());}; // invalid session key .. YOU HAVE DIED
+      if(!test($r,$t)){harakiri(wack());}; // invalid session key .. YOU HAVE DIED
       if(is_dir("$h/$r")){return $r;}; // session is live
       $u=pget("/Proc/keys/$r"); if(!$u){header($z);}; // invalid session key .. YOU HAVE DIED
       $n=mksesn($u); return $n; // if all went well, we are still alive .. all is well
