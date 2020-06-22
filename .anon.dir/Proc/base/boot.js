dc2816d436d5a56c752d527030dfe6241d47c7aa
argon <kindhacker@protonmail.com>
2020-06-22 09:08:04 +0200

    Site anchor fix

--- .anon.dir/Proc/base/boot.js
+++ .anon.dir/Proc/base/boot.js
@@ -341,7 +341,7 @@
                             if(hr.startsWith("#"))
                             {
                                 let trgt=this.cntx.getElementById(hr.slice(1));
-                                if(trgt){trgt.scrollIntoView({behavior:"smooth"})};
+                                if(trgt){trgt.scrollIntoView({behavior:"smooth", block:"nearest"})};
                             };
                         },false);
                     });
