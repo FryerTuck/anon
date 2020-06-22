58001210ebd92ccfee028e8a25aaea052902a46e
argon <kindhacker@protonmail.com>
2020-06-22 12:31:31 +0200

    503 test 4

--- .anon.dir/Proc/base/aard.php
+++ .anon.dir/Proc/base/aard.php
@@ -358,9 +358,9 @@ namespace Anon;
       if($n){unset($_COOKIE[$n]); return;}; // session key expired
       $r=kuki('APIKEY'); if(!$r){$r=post('APIKEY');}; if(!$r){$r=envi('APIKEY');}; if(!$r){return;}; // no key
       if(!test($r,$t)){harakiri(wack());}; // invalid session key .. YOU HAVE DIED
-if(isset($_GET['test'])){die("503 test 3");};
       if(is_dir("$h/$r")){return $r;}; // session is live
       $u=pget("/Proc/keys/$r"); if(!$u&&(envi('INTRFACE')==='GUI')){return;}; // key may have expired
+if(isset($_GET['test'])){die("503 test 4");};
       if(!$u){harakiri(wack());}; // invalid session key .. YOU HAVE DIED
       $n=mksesn($u); return $n; // if all went well, we are still alive .. all is well
    }
