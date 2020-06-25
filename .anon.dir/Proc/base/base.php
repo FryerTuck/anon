002ccc84b94b0e4aa5ef5c92a736cd4669ff1367
argon <kindhacker@protonmail.com>
2020-06-25 18:09:53 +0200

    img.impose fix

--- .anon.dir/Proc/base/base.php
+++ .anon.dir/Proc/base/base.php
@@ -828,7 +828,8 @@ namespace Anon;
         {
             expect::path($pth,[R,F]); $w=null; $h=null; $x=0; $y=0;
             if(isNuma($dim)){$w=$dim[0]; $h=$dim[1];}; if(isNuma($pos)){$x=$pos[0]; $y=$pos[1];};
-            $img=$this->meta->imag; $mrk=(new \Imagick()); $mrk->readImage(path($pth));
+            $img=$this->meta->imag; $mrk=(new \Imagick());
+            $img->setBackgroundColor(new \ImagickPixel('transparent')); $mrk->readImage(path($pth));
             if(!$w){$w=$mrk->getImageWidth();}; if(!$h){$h=$mrk->getImageHeight();};
             $mrk->scaleImage($w,$h); $img->compositeImage($mrk,\Imagick::COMPOSITE_OVER,$x,$y);
         }
