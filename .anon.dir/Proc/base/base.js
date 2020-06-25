dc458e3abf142770ed35490348f096e9db2df6e2
argon <kindhacker@protonmail.com>
2020-06-25 21:19:24 +0200

    render test

--- .anon.dir/Proc/base/base.js
+++ .anon.dir/Proc/base/base.js
@@ -1063,6 +1063,7 @@
          let m,q,t,x; m=r.head.ContentType.split(';')[0].split('/x-').join('/');
          q=m.split('/'); t=trim(q[0]); x=trim(q[1]); if(!isin(keys(parser),t)){t=x};
          if(!trim(r.body)){f("");};
+dump(r);
          parsed(r,t,(z)=>
          {
             if(t=='markdown'){z=create({div:'.markdown-page',contents:[z]})}; f(z);
