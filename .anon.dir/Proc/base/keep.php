184d776959afa0276cb6ebbb77e95fe19dc1a0ee
argon <kindhacker@protonmail.com>
2020-06-30 15:13:29 +0200

    upkeep init test

--- .anon.dir/Proc/base/keep.php
+++ .anon.dir/Proc/base/keep.php
@@ -32,12 +32,13 @@ namespace Anon;
 
      if(isset($_GET['upkeep'])&&($_GET['upkeep']==='init'))
      {
-         if(!isset($_GET['rf'])||!isset($_GET['rk'])){wack(); exit;}; // security
-         $rf=$_GET['rf']; $rp="/$rf"; $rk=$_GET['rk']; $fc=pget($rp,0); $rh=sha1($fc);
-         if(!isin($fc,'$ck = \'{:ck:}\'')||($rh!==$rk)){wack(); exit;}; // security
-         $mp=password_hash(pget("$/Proc/info/pass.inf"),PASSWORD_DEFAULT);
-         pset("$/User/data/master/pass",$mp); $nh=NAVIHOST; void($rp); wait(500);
-         header("Location: $nh"); exit;
+         die("test 1");
+         // if(!isset($_GET['rf'])||!isset($_GET['rk'])){wack(); exit;}; // security
+         // $rf=$_GET['rf']; $rp="/$rf"; $rk=$_GET['rk']; $fc=pget($rp,0); $rh=sha1($fc);
+         // if(!isin($fc,'$ck = \'{:ck:}\'')||($rh!==$rk)){wack(); exit;}; // security
+         // $mp=password_hash(pget("$/Proc/info/pass.inf"),PASSWORD_DEFAULT);
+         // pset("$/User/data/master/pass",$mp); $nh=NAVIHOST; void($rp); wait(500);
+         // header("Location: $nh"); exit;
      };
 
 
