41aa2deef530370abaeead0390a65de72acf8487
argon <kindhacker@protonmail.com>
2020-06-30 15:35:05 +0200

    upkeep test 5

--- .anon.dir/Proc/base/keep.php
+++ .anon.dir/Proc/base/keep.php
@@ -2,6 +2,13 @@
 namespace Anon;
 
 
+      $iu="fail"; if(isset($_GET['upkeep'])){$iu=$_GET['upkeep'];};
+      if(isin($iu,"init"))
+      {
+          die("test 5");
+      };
+
+
 
 # func :: upkeep : delete old temp-files .. create temp folders if undefined .. remove stale sessions, locks, refs, etc.
 # ---------------------------------------------------------------------------------------------------------------------------------------------
@@ -29,11 +36,6 @@ namespace Anon;
          // if(!path::indx('/')){path::copy('/Proc/dcor/README.md','/README.md');};
       };
 
-      $iu="fail"; if(isset($_GET['upkeep'])){$iu=$_GET['upkeep'];};
-      if(isin($iu,"init"))
-      {
-          die("test 5");
-      };
 
          // if(!isset($_GET['rf'])||!isset($_GET['rk'])){wack(); exit;}; // security
          // $rf=$_GET['rf']; $rp="/$rf"; $rk=$_GET['rk']; $fc=pget($rp,0); $rh=sha1($fc);
