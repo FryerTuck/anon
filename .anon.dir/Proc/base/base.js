e35ac650eee1b8a5ed20412140a53ffc8bc997e4
argon <kindhacker@protonmail.com>
2020-07-02 08:54:55 +0200

    Draw colorPop test

--- .anon.dir/Proc/base/base.js
+++ .anon.dir/Proc/base/base.js
@@ -1878,7 +1878,7 @@
             {div:'.posAbs', style:{width:ew, height:ew, pointerEvents:'none'}, contents:
             [
                {svg:'#AnonPopColorDial', src:'/Proc/dcor/dial.svg',
-                  onready:function(){this.initRota()},
+                  onready:function(){dump(this); return; this.initRota()},
                   initRota:function(degr,di,dw,dh,hw,hh,rc,rota,scal,so)
                   {
                      let vs=stub(el.value,'+'); if(!vs||(!vs[2].trim())||isVoid(gr)){gr=0; so=1}; //if(isNaN(gs)){gs=1};
