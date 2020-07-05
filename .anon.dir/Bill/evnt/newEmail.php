996a61e333a41cde7d669c756cd5b0bbad7d1bd1
argon <kindhacker@protonmail.com>
2020-07-05 16:30:47 +0200

    Bill auto-add firm to contacts on new mail

--- .anon.dir/Bill/evnt/newEmail.php
+++ .anon.dir/Bill/evnt/newEmail.php
@@ -5,9 +5,32 @@ $export=function($v)
 {
    $fa=$v->fromAddy; $sp=$v->savePath; $am='autoMail'; $bn=find::firmByMail($fa);
    if(!isee("$sp/business")){path::make("$sp/business",$bn);}; // assign a business to email
-   $ba=conf("Bill/$am"); if(!$ba){$ba=conf("Proc/$am");};
-   $da=$v->destAddy; $i=path::info($ba); $ba="$i->user@$i->host"; // billing address
 
+
+   $pl=plug("sqlite::$/Bill/data/contacts/");
+   $ex=$pl->select
+   ([
+       using => "mailFirm",
+       fetch => "firm",
+       where => "mail = $fa",
+   ]);
+
+   if(span($ex)<1)
+   {
+       $pl->insert
+       ([
+           using => "mailFirm",
+           write =>
+           [
+               "mail" => $fa,
+               "firm" => $bn,
+           ],
+       ]);
+   };
+
+
+   $ba=conf("Bill/$am"); if(!$ba){$ba=conf("Proc/$am");}; // autoMail
+   $da=$v->destAddy; $i=path::info($ba); $ba="$i->user@$i->host"; // billing address
    if($da!==$ba){return;}; // not meant for billing
 
    // Do stuff here with billing
