ca2c5f70f7d1b56ec5e0091913cd2ae68f784d24
argon <kindhacker@protonmail.com>
2020-07-05 22:46:21 +0200

    Data treeMenu fix

--- .anon.dir/Data/aard.php
+++ .anon.dir/Data/aard.php
@@ -53,7 +53,7 @@ class Data
       {
          $sp="$so->path/data"; if(!isFold($sp)){continue;};
          // $hd=path::ogle([using=>$sp,fetch=>$cn,where=>['type = fold']]);
-         $so->data=[path::tree($sp)];
+         $so->data=path::tree($sp)->data;
          // foreach($so->data->data as $dk => $dv)
          // {
          //    signal::dump($dv);
