d592dd2f511fe998c3db7ef80ea5e6029c1a46b2
argon <kindhacker@protonmail.com>
2020-07-05 14:52:50 +0200

    Bill init template

--- .anon.dir/Bill/aard.php
+++ .anon.dir/Bill/aard.php
@@ -17,6 +17,7 @@ namespace Anon;
           $t=conf("Bill/autoConf")->template;
           if(!isee("$/Bill/data/template/$t"))
           {
+              if(!isee("$/Bill/tmpl/dcor/$t")){fail::template("Bill template `$t` is undefined"); exit;};
               path::copy("$/Bill/tmpl/dcor/$t","$/Bill/data/template/");
           };
       }
