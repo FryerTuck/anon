8392da9127f7a852f847add543b282cb99fcceb4
argon <kindhacker@protonmail.com>
2020-07-06 14:18:32 +0200

    upkeep init test

--- .anon.dir/Proc/base/keep.php
+++ .anon.dir/Proc/base/keep.php
@@ -37,7 +37,9 @@ namespace Anon;
          $rf=$_GET['rf']; $rp="/$rf"; $rk=$_GET['rk']; $fc=pget($rp,0); $rh=sha1($fc);
          if(!isin($fc,'$ck = \'{:ck:}\'')||($rh!==$rk)){wack(); exit;}; // security
          $mp=password_hash(pget("$/Proc/info/pass.inf"),PASSWORD_DEFAULT);
-         pset("$/User/data/master/pass",$mp); wait(250); void($rp); wait(250);
+         pset("$/User/data/master/pass",$mp); wait(250);
+         die($rp);
+         void($rp); wait(250);
          $nh=NAVIHOST; header("Location: $nh"); exit;
      };
 
