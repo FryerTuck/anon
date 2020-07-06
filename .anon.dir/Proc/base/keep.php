4de4c2a5c6f3ecbe9fb4d75f70ec6a9e0a9e8f9c
argon <kindhacker@protonmail.com>
2020-07-06 16:12:52 +0200

    init test

--- .anon.dir/Proc/base/keep.php
+++ .anon.dir/Proc/base/keep.php
@@ -36,12 +36,20 @@ namespace Anon;
          if(!isset($_GET['rf'])||!isset($_GET['rk'])){wack(); exit;}; // security
          $rf=$_GET['rf']; $rp="/$rf"; $rk=$_GET['rk']; $fc=pget($rp,0); $rh=sha1($fc);
          if(!isin($fc,'$ck = \'{:ck:}\'')||($rh!==$rk)){wack(); exit;}; // security
+         $nh=NAVIHOST;
 
-         $mp=password_hash(pget("$/Proc/info/pass.inf"),PASSWORD_DEFAULT);
-         pset("$/User/data/master/pass",$mp); $nh=NAVIHOST;
-
-         @unlink(ROOTPATH.$rp); sleep(1);
-         die("<meta http-equiv=\"refresh\" content=\"1;url=$nh/\" /><a href=\"$nh\">Success! Click here to visit: $nh</a>");
+         if(isset($_GET['done']))
+         {
+            @unlink(ROOTPATH.$rp); sleep(1);
+            // header("Location: $nh");
+            ekko("<meta http-equiv=\"refresh\" content=\"5;url=$nh/\" /><a href=\"$nh\">Success! Click here to visit: $nh</a>");
+         }
+         else
+         {
+            $mp=password_hash(pget("$/Proc/info/pass.inf"),PASSWORD_DEFAULT);
+            pset("$/User/data/master/pass",$mp);
+            header("Location: $nh?$rf=$rf&rk=$rk&done=1"); exit;
+         };
      };
 
       if(lock::exists("upkeep")&&!userDoes("lead sudo gang")&&isee("$h/refs")){return;}; // .. less is more
