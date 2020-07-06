e80ac463e96bdda0a87086f15c84593798fbc807
argon <kindhacker@protonmail.com>
2020-07-06 04:42:05 +0200

    showHyperConduit test2

--- .anon.dir/Proc/base/base.php
+++ .anon.dir/Proc/base/base.php
@@ -80,8 +80,7 @@ namespace Anon;
       if(!isText($v)<5){return;}; $v=crop($v); $s=stub($v,['::','://']);
       if($s)
       {
-         $o=$s[0]; $p=$s[2]; $x=path::part($p); if(!$x){return;}; $c="$o::$x->path"; $p=$x->fork;
-signal::dump($x);
+         $o=$s[0]; $p=$s[2]; $x=path::part($p); signal::dump($p); if(!$x){return;}; $c="$o::$x->path"; $p=$x->fork;
          if(!isPath($p)){$p=null;}; $r=knob(['plug'=>$c,'path'=>$p]); return ($w?$r:(!$p?$c:($c.$p)));
       };
       if(!isPath($v)){return;}; if((substr($v,-4,4)!=='.url')&&!strpos($v,'.url/')){return;};
