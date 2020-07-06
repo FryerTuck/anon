427746c9cb026727fbe828dc0528954cf5a67355
argon <kindhacker@protonmail.com>
2020-07-06 21:21:29 +0200

    added clans

--- .anon.dir/User/boot.js
+++ .anon.dir/User/boot.js
@@ -94,7 +94,7 @@
                                   purl(`/User/initConf`,{pass:pw,mail:em},(r)=>
                                   {
                                       r=r.body; if(r!=OK){return}; let m;
-                                      m=`Now create a power-user that belongs to (at least) these clans:\n\`work sort geek sudo\`\n`+
+                                      m=`Now create a power-user that belongs to (at least) these clans:\n\`work,sort,bill,draw,geek,mind,gang,sudo\`\n`+
                                         `For help on this, type \`help user\` in the terminal and hit Enter on your keyboard.`;
                                       popAlert(`thumbs-up :: Success! : Initial config set.\n\n${m}`);
                                       this.root.exit();
