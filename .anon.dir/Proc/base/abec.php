b6fce0127afa627b4fb4958bdd2c50e6b7e02012
argon <kindhacker@protonmail.com>
2020-07-06 22:38:07 +0200

    path::info fix

--- .anon.dir/Proc/base/abec.php
+++ .anon.dir/Proc/base/abec.php
@@ -613,7 +613,7 @@ namespace Anon;
       {
          if(!isText($d,1)){fail('expecting non-empty text');}; $n=null; $h=HOSTNAME;
          if(path($d)){$p=crop($d); if(frst($p)==='$'){$p=substr($p,1);}; $d="file://{$h}{$p}";};
-         if(isin($d,'::')){$s=stub($d,'::'); $p=crop($s[2]); if(frst($p)!=='/'){$p="/$p";}; $d="{$s[0]}://{$h}{$p}";};
+         if(isin($d,'::')){$s=stub($d,'::'); $p=crop($s[2]); if(frst($p)==='$'){$p=$p=substr($p,1);}; $d="{$s[0]}://{$h}{$p}";};
          $i=knob(parse_url($d)); $p=$i->path; $q=$i->query;
          if(!$i->scheme||!$i->host){if(!$fail){return;}; fail('expecting valid path-string -or URL-string');}; $x=stub($d,['#','&','?','@',':']);
          if(isin($d,'@')){$x=stub($d,'@')[0]; if(isin($x,['#','&','?'])){fail('invalid URL');}};
