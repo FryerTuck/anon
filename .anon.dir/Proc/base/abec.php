adff1494d72cb21d3d7dd12388a29b24809dea83
argon <kindhacker@protonmail.com>
2020-07-13 06:13:48 +0200

    path::copy fix

--- .anon.dir/Proc/base/abec.php
+++ .anon.dir/Proc/base/abec.php
@@ -791,7 +791,7 @@ namespace Anon;
 
       static function copy($pf,$pt)
       {
-         $of=path($of); $ot=path($ot); if(!$of||!$ot){fail('expecting 2 paths');}; if(!isee($of)){fail("`$of` is undefined");};
+         $of=path($pf); $ot=path($pt); if(!$of||!$ot){fail('expecting 2 paths');}; if(!isee($of)){fail("`$of` is undefined");};
          $tx=isee($ot); if(!$tx){$np=(isFold($of)?$ot:self::twig($ot)); pset("$np/");}; if(last($pf)==="/"){$of.="/.";};
          lock::awaits($ot); exec::{"cp -R $of $ot"}(); lock::remove($ot); return true; // will fail if not OK
       }
