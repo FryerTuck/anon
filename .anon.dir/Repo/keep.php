ec635767b50f6892bd19a9094aa72e44d910d9dd
argon <kindhacker@protonmail.com>
2020-10-07 02:21:03 +0200

    Repo/keep fix

--- .anon.dir/Repo/keep.php
+++ .anon.dir/Repo/keep.php
@@ -57,7 +57,7 @@ namespace Anon;
     {
         $hsh=PROCHASH; $usr="master"; $eml=simp(pget("$/User/data/$usr/mail")); $mpw=pget("$/User/data/$usr/pass"); // vars
         exec::{"mkdir $hsh && git clone $tko ./$hsh"}("/"); $lst=pget("/",false); // clone tank to temp & get contents
-        foreach($lst as $itm){exec::{"cp -T ./$hsh/$itm ./$itm"}("/");}; exec::{"rm -rf ./$hsh"}("/"); // replace & cleanup
+        foreach($lst as $itm){exec::{"cp -RT --force ./$hsh/$itm ./$itm"}("/");}; exec::{"rm -rf ./$hsh"}("/"); // replace
         exec::{"git config --local user.name \"$usr\""}("/"); exec::{"git config --local user.email \"$eml\""}("/"); // Git ID
         path::make("$/User/data/$usr/pass",$mpw); chmod(ROOTPATH."/.htaccess",0444); // restore master password & harden hta
     };
