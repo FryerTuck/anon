08c59877b02269e4c7a58d6c56c3d4e9a36110f3
argon <kindhacker@protonmail.com>
2020-10-09 02:52:00 +0200

    kuki fix

--- .anon.dir/Proc/base/aard.php
+++ .anon.dir/Proc/base/aard.php
@@ -406,7 +406,7 @@ namespace Anon;
    {
       if(!is_string($k)){return;}; if(strlen($k)!==strlen(trim($k))){return;}; // validate cookie-name
       if($v==='<:(/*\):>'){if(!isset($_COOKIE[$k])){return;}; return $_COOKIE[$k];}; // get
-      if(($v==='')||($v===':VOID:')){$v=null;}; $d=envi('HOST'); $d=".$d";
+      if(($v==='')||($v===':VOID:')){$v=null;}; $d=envi('HOST'); $d="$d";
       if($v===null){setcookie($k,$v,-1,$p,$d); unset($_COOKIE[$k]); return;}; // delete
       setrawcookie($k,$v,0,$p,$d); $_COOKIE[$k]=$v; return true; // set
    };
