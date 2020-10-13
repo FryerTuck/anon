de8c8feb5238a7243f859771d21a8c0fc4557ef7
argon <kindhacker@protonmail.com>
2020-10-13 10:02:02 +0200

    fuse-test fix

--- .anon.dir/Navi/tool/test_fused_site/aard.js
+++ .anon.dir/Navi/tool/test_fused_site/aard.js
@@ -7,13 +7,13 @@ extend(Anon.Navi.tool)
 ({
     test_fused_site:function(tab, panl)
     {
-        // Anon.Navi.vars.RECEIVER=cookie.select(`RECEIVER`);
-        // cookie.update(`RECEIVER`,`anon`); cookie.create(`FUSETEST`,`yes`);
-        // select('#NaviTabber').listen('close',function()
-        // {
-        //     cookie.update(`RECEIVER`,Anon.Navi.vars.RECEIVER);
-        //     cookie.delete(`FUSETEST`);
-        // });
+        // Anon.Navi.vars.RECEIVER=cookie.select(`RECEIVER`); cookie.update(`RECEIVER`,`anon`);
+        cookie.create(`FUSETEST`,`yes`);
+        select('#NaviTabber').listen('close',function()
+        {
+            // cookie.update(`RECEIVER`,Anon.Navi.vars.RECEIVER);
+            cookie.delete(`FUSETEST`);
+        });
 
         panl=tab.body.select(`.NaviViewPanl`)[0];
         panl.insert({panl:`.posAbs`, $:
