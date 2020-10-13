a1420f24d71bd3e6dc27dd52fbb3092702a1f931
argon <kindhacker@protonmail.com>
2020-10-13 12:37:46 +0200

    Navi/test_fused_site view fix

--- .anon.dir/Navi/tool/test_fused_site/aard.js
+++ .anon.dir/Navi/tool/test_fused_site/aard.js
@@ -7,14 +7,8 @@ extend(Anon.Navi.tool)
 ({
     test_fused_site:function(tab, panl)
     {
-        // Anon.Navi.vars.RECEIVER=cookie.select(`RECEIVER`); cookie.update(`RECEIVER`,`anon`);
-        select('#NaviTabber').listen('close',function()
-        {
-            // cookie.update(`RECEIVER`,Anon.Navi.vars.RECEIVER);
-        });
-
         panl=tab.body.select(`.NaviViewPanl`)[0];
-        panl.insert({panl:`.posAbs`, $:
+        panl.insert({layr:
         [
             {iframe:`.spanFull`, src:(HOSTPURL+'/?ANONFUSETEST')}
         ]});
